ab7dbe59ed28b4d5c6023884572d64ef0e3b5015
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-05-16 08:43:16 +0300

    init_css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,3 @@
-import css from "./App.module.css";
-import Cafeinfo from "../Cafeinfo/Cafeinfo";
-// import { useState } from "react";
-// import type { VoteType } from "../../types/votes";
 
-export default function App() {
-
-    // const [votes, setVotes] = useState({
-    //     good: 0,
-    //     neutral: 0,
-    //     bad: 0,
-    // });
-
-    // const handleVote = (type: VoteType) => {
-    //     setVotes
-
-    // };
-
-
-
-
-
-
-
-
-
-    return <div className={css.app}>
-      <Cafeinfo/>
-  </div>;
-}
 
 
