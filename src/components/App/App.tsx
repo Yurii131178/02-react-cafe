c069e9913abf59ce6d5894b6490552bf88e70211
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-05-17 22:43:53 +0300

    add Notification function and set condition for reset button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { type Votes, type VoteType } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
-
+import Notification from "../Notification/Notification";
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
     good: 0,
@@ -32,12 +32,21 @@ export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
-      <VoteStats
-        votes={votes}
-        totalVotes={totalVotes}
-        positiveRate={positiveRate}
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
       />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
     </div>
   );
 }
